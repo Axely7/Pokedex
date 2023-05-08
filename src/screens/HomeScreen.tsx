26624f72f23dfe64e49574bef390df8c0d65edfa
Axel Jimenez <axely7@hotmail.com>
2023-05-08 14:40:46 -0600

    getting pokemons with api

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Image, Text, View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {usePokemonPaginated} from '../hooks/usePokemonPaginated';
+import {styles} from '../theme/appTheme';
 
 export const HomeScreen = () => {
+  const {top} = useSafeAreaInsets();
+  const {isLoading, simplePokemonList} = usePokemonPaginated();
+  console.log(simplePokemonList);
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <>
+      <Image
+        source={require('../assets/pokebola.png')}
+        style={styles.pokebolaBG}
+      />
+      <Text style={{...styles.title, ...styles.globalMargin, top: top + 20}}>
+        Pokedex
+      </Text>
+    </>
   );
 };
