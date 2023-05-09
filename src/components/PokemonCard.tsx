915110711cfd45b46abb185aa8af823fc6da597c
Axel Jimenez <axely7@hotmail.com>
2023-05-09 10:42:08 -0600

    mounting flatlist and creating pokemon cards

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import {
+  Dimensions,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {SimplePokemon} from '../interfaces/pokemonInterfaces';
+
+const windowWidth = Dimensions.get('window').width;
+
+interface Props {
+  pokemon: SimplePokemon;
+}
+
+export const PokemonCard = ({pokemon}: Props) => {
+  return (
+    <TouchableOpacity activeOpacity={0.7}>
+      <View style={{...styles.cardContainer, width: windowWidth * 0.4}}>
+        <View>
+          <Text style={styles.name}>
+            {pokemon.name}
+            {'\n#' + pokemon.id}
+          </Text>
+        </View>
+        <Image
+          source={require('../assets/pokebola-blanca.png')}
+          style={styles.pokebola}
+        />
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    marginHorizontal: 10,
+    backgroundColor: 'red',
+    height: 120,
+    width: 160,
+    marginBottom: 25,
+    borderRadius: 10,
+  },
+  name: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    top: 20,
+    left: 10,
+  },
+  pokebola: {
+    width: 100,
+    height: 100,
+    position: 'absolute',
+    bottom: -20,
+    right: -20,
+    opacity: 0.5,
+  },
+});
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import {Image, Text, View} from 'react-native';
+import {ActivityIndicator, FlatList, Image, Text, View} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {FadeInImage} from '../components/FadeInImage';
+import {PokemonCard} from '../components/PokemonCard';
 import {usePokemonPaginated} from '../hooks/usePokemonPaginated';
 import {styles} from '../theme/appTheme';
 
 export const HomeScreen = () => {
   const {top} = useSafeAreaInsets();
-  const {isLoading, simplePokemonList} = usePokemonPaginated();
-  console.log(simplePokemonList);
+  const {isLoading, simplePokemonList, loadPokemons} = usePokemonPaginated();
 
   return (
     <>
@@ -15,9 +16,32 @@ export const HomeScreen = () => {
         source={require('../assets/pokebola.png')}
         style={styles.pokebolaBG}
       />
-      <Text style={{...styles.title, ...styles.globalMargin, top: top + 20}}>
-        Pokedex
-      </Text>
+
+      <View style={{alignItems: 'center'}}>
+        <FlatList
+          data={simplePokemonList}
+          ListHeaderComponent={
+            <Text
+              style={{
+                ...styles.title,
+                ...styles.globalMargin,
+                top: top + 20,
+                marginBottom: top + 20,
+              }}>
+              Pokedex
+            </Text>
+          }
+          keyExtractor={pokemon => pokemon.id}
+          showsVerticalScrollIndicator={false}
+          numColumns={2}
+          renderItem={({item}) => <PokemonCard pokemon={item} />}
+          onEndReached={loadPokemons}
+          onEndReachedThreshold={0.4}
+          ListFooterComponent={
+            <ActivityIndicator style={{height: 100}} size={20} color="grey" />
+          }
+        />
+      </View>
     </>
   );
 };
