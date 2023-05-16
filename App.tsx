2c0fb0f50f78a4905c51437e8f3a219590385eeb
Axel Jimenez <axely7@hotmail.com>
2023-05-16 10:29:25 -0600

    detail Screen done and applying bottom tabs

--- App.tsx
+++ App.tsx
@@ -1,14 +1,13 @@
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {Text} from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
-import {Navigator} from './src/navigator/Navigator';
+import {Tabs} from './src/navigator/Tabs';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Navigator />
+      {/* <Navigator /> */}
+      <Tabs />
     </NavigationContainer>
   );
 };
