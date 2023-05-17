6e251acf2874e261b06a45e3f19eb04d2609369f
Axel Jimenez <axely7@hotmail.com>
2023-05-17 10:27:06 -0600

    searchbar component and listing pokemons

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,10 +1,48 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Platform, FlatList} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {Loading} from '../components/Loading';
+import {PokemonCard} from '../components/PokemonCard';
+import {SearchInput} from '../components/SearchInput';
+import {usePokemonSearch} from '../hooks/usePokemonSearch';
+import {styles} from '../theme/appTheme';
 
 export const SearchScreen = () => {
+  const {top} = useSafeAreaInsets();
+
+  const {isFetching, simplePokemonList} = usePokemonSearch();
+
+  if (isFetching) {
+    return <Loading />;
+  }
+
   return (
-    <View>
-      <Text>Search Screen</Text>
+    <View
+      style={{
+        flex: 1,
+        marginTop: Platform.OS === 'ios' ? top : top + 10,
+        marginHorizontal: 20,
+      }}>
+      <SearchInput />
+
+      <FlatList
+        data={simplePokemonList}
+        ListHeaderComponent={
+          <Text
+            style={{
+              ...styles.title,
+              ...styles.globalMargin,
+
+              paddingBottom: 10,
+            }}>
+            Pokedex
+          </Text>
+        }
+        keyExtractor={pokemon => pokemon.id}
+        showsVerticalScrollIndicator={false}
+        numColumns={2}
+        renderItem={({item}) => <PokemonCard pokemon={item} />}
+      />
     </View>
   );
 };
